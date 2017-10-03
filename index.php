3a2f1fe0962e811a37ec098ad7192684db1a12c7
Chhavi Inani <ci38@afsconnect1.njit.edu>
2017-10-03 12:49:19 -0400

    added index.php

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
 $date =  date('Y-m-d', time());
 echo "The value of \$date: ".$date."<br>";
+$date = str_replace('-', '/', $date);
 
 $tar = "2017/05/24";
 echo "The value of \$tar: ".$tar."<br>";
 
 $year = array("2012", "396", "300","2000", "1100", "1089");
 echo "The value of \$year: ";
-print_r($year)
+print_r($year);
 
 ?>
